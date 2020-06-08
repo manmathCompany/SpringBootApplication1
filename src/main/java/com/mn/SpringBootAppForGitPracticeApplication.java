e2af1145a0b20d513cdfcdb90a217d8b56bbb448
TL <TL@gmail.com>
2020-06-08 14:06:50 +0530

    first delpoyement

---
+++ src/main/java/com/mn/SpringBootAppForGitPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppForGitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppForGitPracticeApplication.class, args);
+	}
+
+}
